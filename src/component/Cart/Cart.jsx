934223206fc4d021d9411f674ef5a5887177e09f
imran2024bd <imranitbd34@gmail.com>
2024-07-04 17:25:06 +0600

    PropTypes Warning starting

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import './Cart.css'
 
 const Cart = ({ cart }) => {
@@ -5,10 +6,12 @@ const Cart = ({ cart }) => {
         <div >
             <h4>Cart: {cart.length} </h4>
             <div className="cart-container">
-                {cart.map(bottle => <img src={bottle.img}></img>)}
+                {cart.map(bottle => <img key={bottle.id} src={bottle.img}></img>)}
             </div>
         </div>
     );
 };
-
+Cart.propTypes = { cart: PropTypes.array.isRequired }
 export default Cart;
+
+// 41 - 8[Optional] Display Saved Cart From Local Storage
